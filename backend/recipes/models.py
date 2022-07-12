31277b888bb59dc9e26b83aac87e361ed8c5e717
Игорь Живов <izhivov97@yandex.ru>
2022-07-12 23:29:49 +0300

    Начал писать модель рецептов

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from ..users.models import User
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        verbose_name='Автор рецепта'
+    )
+    title = models.CharField(
+        max_length=255,
+        blank=False,
+        verbose_name='Название рецепта'
+    )
+    image = models.ImageField(
+        upload_to='recipes/',
+        blank=True,
+        verbose_name='Изображение блюда'
+    )
+    description = models.TextField(
+        'Описание',
+        help_text='Введите текст описания рецепта'
+    )
+    cooking_time = models.PositiveIntegerField('Время приготовления')
