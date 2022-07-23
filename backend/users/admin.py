d75f56c4a8a81c1fca9d05d1d35f681cd47a3de8
Игорь Живов <izhivov97@yandex.ru>
2022-07-23 22:33:09 +0300

    Зарегистрировал в админке модели CustomUser и Follow

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser, Follow
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'email', 'username', 'first_name', 'last_name', 'is_blocked',
+        'is_superuser',
+    )
+    list_filter = (
+        'email', 'username', 'is_blocked', 'is_superuser',
+    )
+    search_fields = ('email', 'username', 'first_name', 'last_name',)
+    fieldsets = (
+        (None, {'fields': (
+            'email', 'username', 'first_name', 'last_name', 'password',
+        )}),
+        ('Permissions', {'fields': ('is_blocked', 'is_superuser',)})
+    )
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+    search_fields = ('user', 'author')
