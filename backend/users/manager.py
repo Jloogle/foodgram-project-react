9c16b76fbced1cd25d042ccfd43cefd2b0f268ad
Игорь Живов <izhivov97@yandex.ru>
2022-07-23 22:24:10 +0300

    Кастомизмровал менеджер создания юзера и суперюзера, в юзере сделал проверку на отсутствие email

---
+++ backend/users/manager.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password, username, **extra_fields):
+        if not email:
+            raise ValueError('Поле email не может быть пустым')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, username, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, username, **extra_fields)
