58ef88a52e9c7f5ff6e521c8a0f90ce90e826277
Игорь Живов <izhivov97@yandex.ru>
2022-07-24 13:17:30 +0300

    Добавил поле цвета в модель Tag

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -16,6 +16,14 @@ class Tag(models.Model):
         blank=False,
         unique=True
     )
+    color = models.CharField('Цвет тега в HEX', max_length=7)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
@@ -24,32 +32,36 @@ class Ingredient(models.Model):
         max_length=100,
         blank=False
     )
-    count = models.PositiveIntegerField(
-        'Количество ингредиентов',
-        blank=False
-    )
+    count = models.PositiveIntegerField()
     measurement_unit = models.CharField(
         'Единица измерения',
-        max_length=100
+        max_length=100,
+        blank=False
     )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'measurement_unit'],
+                name='unique ingredients'
+            )
+        ]
 
 
 class Recipe(models.Model):
     author = models.ForeignKey(
         CustomUser,
-        on_delete=models.SET_NULL,
-        null=True,
+        on_delete=models.CASCADE,
         verbose_name='Автор рецепта',
-        related_name='recipes'
+        related_name='recipes',
+        db_index=True
     )
     title = models.CharField(
         'Название рецепта',
         max_length=250,
-    )
-    pub_date = models.DateTimeField(
-        'Дата публикации',
-        auto_now_add=True,
-        db_index=True
+        blank=False
     )
     image = models.ImageField(
         'Изображение блюда',
@@ -58,7 +70,8 @@ class Recipe(models.Model):
     )
     description = models.TextField(
         'Описание',
-        help_text='Введите описания рецепта'
+        help_text='Введите описания рецепта',
+        blank=False
     )
     cooking_time = models.PositiveIntegerField(
         'Время приготовления',
@@ -71,7 +84,8 @@ class Recipe(models.Model):
         verbose_name='Тег'
     )
 
+
     class Meta:
-        ordering = ['-pub_date']
+        ordering = ['-pk']
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
