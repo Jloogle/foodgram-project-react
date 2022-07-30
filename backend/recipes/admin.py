56b5fb9194eab7d57c6e8d567dd88da628a307d3
Игорь Живов <izhivov97@yandex.ru>
2022-07-30 20:04:48 +0300

    Зарегистрировал в админке модели рецептов

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Recipe, RecipeIngredient, Ingredient, ShoppingCart, Favorite
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'color')
+    list_filter = ('name', 'slug')
+    search_fields = ('name',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit')
+    list_filter = ('name', 'measurement_unit')
+    search_fields = ('name',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'count_favorites')
+    list_filter = ('author', 'name', 'tags')
+
+    def count_favorites(self, obj):
+        return obj.favorite.count()
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'ingredient', 'amount')
+    list_filter = ('recipe', 'ingredient', 'amount')
+    search_fields = ('recipe', 'ingredient')
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'user')
+    list_filter = ('recipe', 'user')
+    search_fields = ('recipe', 'user')
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'user')
+    list_filter = ('recipe', 'user')
+    search_fields = ('recipe', 'user')
