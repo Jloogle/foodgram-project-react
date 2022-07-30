18d6d28a584ea7c83a06afc0018c750d8283084d
Игорь Живов <izhivov97@yandex.ru>
2022-07-30 20:06:28 +0300

    подправил сериализаторы для пользователя

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,15 +1,13 @@
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 
-from .models import CustomUser, Follow
-from .mixins import SubscribeMixin
-
-User = CustomUser
+from users.mixins import SubscribeMixin
+from users.models import CustomUser
 
 
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta:
-        model = User
+        model = CustomUser
         fields = (
             'email', 'id', 'username', 'password',  'first_name', 'last_name'
         )
@@ -19,22 +17,9 @@ class CustomUserCreateSerializer(UserCreateSerializer):
 
 
 class CustomUserSerializer(UserSerializer, SubscribeMixin):
+    is_subscribed = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ('username', 'first_name', 'id',
                   'last_name', 'email', 'is_subscribed', )
-
-
-class SubscriptionSerializer(CustomUserSerializer):
-    # recipes = serializers.SerializerMethodField()
-    recipes_count = serializers.SerializerMethodField()
-
-    class Meta(CustomUserSerializer.Meta):
-        fields = CustomUserSerializer.Meta.fields + ('recipes_count',)
-
-    # def get_recipes(self):
-    #     pass
-
-    def get_recipes_count(self, obj):
-        return obj.recipes.count()
