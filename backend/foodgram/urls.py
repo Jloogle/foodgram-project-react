42abf45cdb69961046fc6ac3672053f11810f4cc
Игорь Живов <izhivov97@yandex.ru>
2022-07-30 20:07:40 +0300

    подключил урлы для рецептов

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -5,7 +5,7 @@ from django.urls.conf import include
 
 api = [
     path('', include('users.urls', namespace='users')),
-    # path('', include('recipes.urls', namespace='recipes')),
+    path('', include('recipes.urls', namespace='recipes')),
 ]
 
 urlpatterns = [
