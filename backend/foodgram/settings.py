8ade8414d907f2c096edc651d6cba98ed20c4faf
Игорь Живов <izhivov97@yandex.ru>
2022-08-04 21:53:17 +0300

    debug=False

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -18,7 +18,7 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
