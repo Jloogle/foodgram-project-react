7035883043428ee6609bc84adaf8239e4348dcf8
Игорь Живов <izhivov97@yandex.ru>
2022-08-05 17:22:44 +0300

    убрал дефолтные фильтры

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -120,10 +120,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ]
+    )
 }
 
 DJOSER = {
