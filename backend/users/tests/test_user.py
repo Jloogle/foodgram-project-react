b0b9027e4ad9477b5d19084e0575cc713d0757ca
Игорь Живов <izhivov97@yandex.ru>
2022-09-07 12:20:16 +0300

    Немного поменял url в тестах и начал писать тест на удаление аккаунта

--- backend/users/tests/test_user.py
+++ backend/users/tests/test_user.py
@@ -7,9 +7,18 @@ from users.views import CustomUserViewSet
 
 
 class UserTests(APITestCase):
-    def test_create_delete_user(self):
+    def setUp(self) -> None:
+        CustomUser.objects.create(
+            email='test@test.ru',
+            username='User1',
+            password='password',
+            first_name='Test1_first',
+            last_name='Test1_last'
+        )
+
+    def test_create_delete_user(self) -> None:
         """тест создания юзера"""
-        url = 'http://127.0.0.1:8000/api/users/'
+        url = reverse('users:users-list')
         data = {
             'email': 'lora@lora.ru',
             'username': 'LoraFirst',
@@ -19,8 +28,17 @@ class UserTests(APITestCase):
             }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(CustomUser.objects.count(), 1)
-        self.assertEqual(CustomUser.objects.get().username, 'LoraFirst')
+        self.assertEqual(CustomUser.objects.count(), 2)
+        self.assertEqual(CustomUser.objects.get(id=2).username, 'LoraFirst')
+
+        user = CustomUser.objects.get(username='LoraFirst')
+        url = reverse('users:users-detail', kwargs={'id': user.id})
+
+        self.client.force_authenticate(user=user)
+        response = self.client.delete(url)
+        print(response)
+        # print(response)
+        # self.assertEqual(CustomUser.objects.count(), 1)
 
         # user = CustomUser.objects.get(username='LoraFirst')
         # self.client.force_login(user)
